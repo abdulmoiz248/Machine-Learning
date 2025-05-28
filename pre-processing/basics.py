9482f7435817fd63c773dfc0f2ec8c1bd578a173
Abdul Moiz <moiz20920@gmail.com>
2025-05-28 15:05:58 +0500

    covered lavel enconding

--- pre-processing/basics.py
+++ pre-processing/basics.py
@@ -1,20 +1,10 @@
 import pandas as pd
 from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
 
 print('=' * 100)
 print('[INFO] Reading CSV...')
 df = pd.read_csv('./datasets/survey.csv')
-
-print('=' * 100)
-print('[INFO] Total Null Values Per Column:')
-print(df.isna().sum())
-print('Shape =', df.shape)
-
-print('=' * 100)
-print('[INFO] Percentage of Null Values in Dataset:')
-nullPercentage = (df.isna().sum().sum() / (df.shape[0] * df.shape[1])) * 100
-print('Percentage =', round(nullPercentage, 2), '%')
 
 print('=' * 100)
 print('[INFO] Dropping "state" column...')
@@ -23,34 +13,39 @@ df.drop(columns=['state'], inplace=True)
 print('=' * 100)
 print('[INFO] Filling missing values in "self_employed" column with mode...')
 modeVal = df['self_employed'].mode()[0]
-print('Mode =', modeVal)
 df.loc[:, 'self_employed'] = df['self_employed'].fillna(modeVal)
 
 print('=' * 100)
-print('[INFO] Applying SimpleImputer on "work_interfere" with most_frequent strategy...')
-print('Column DataTypes:\n', df.dtypes)
-print('Integer Columns:', df.select_dtypes(include='int').columns.tolist())
-
+print('[INFO] Imputing "work_interfere" with most frequent...')
 si = SimpleImputer(strategy='most_frequent')
 df['work_interfere'] = si.fit_transform(df[['work_interfere']]).ravel()
 
-print('[INFO] Imputation complete.')
 print('=' * 100)
+print('[INFO] Label encoding ordinal columns...')
 
-ohe = OneHotEncoder(drop='first')
-enData = ['self_employed', 'family_history', 'treatment', 'work_interfere', 'remote_work',
+
+
+la=LabelEncoder()
+df['work_interfere'] = la.fit_transform(df['work_interfere'])
+df['leave']=la.fit_transform(df['leave'])
+print('[INFO] Label encoding done.')
+
+print('=' * 100)
+print('[INFO] One hot encoding categorical columns...')
+
+ohe = OneHotEncoder(drop='first', sparse_output=False)
+enData = ['self_employed', 'family_history', 'treatment', 'remote_work',
 'tech_company', 'benefits', 'care_options', 'wellness_program', 'seek_help',
-'anonymity', 'leave', 'mental_health_consequence', 'phys_health_consequence',
+'anonymity', 'mental_health_consequence', 'phys_health_consequence',
 'coworkers', 'supervisor', 'mental_health_interview', 'phys_health_interview',
 'mental_vs_physical', 'obs_consequence']
 
 encoded = ohe.fit_transform(df[enData])
-encodedDf = pd.DataFrame(encoded.toarray(), columns=ohe.get_feature_names_out(enData), index=df.index)
+encodedDf = pd.DataFrame(encoded, columns=ohe.get_feature_names_out(enData), index=df.index)
 
 df.drop(columns=enData, inplace=True)
 df = pd.concat([df, encodedDf], axis=1)
 
-print('[INFO] One hot encoding completed.')
+print('[INFO] Encoding complete.')
 print('=' * 100)
 
-print('df \n' , encodedDf)
